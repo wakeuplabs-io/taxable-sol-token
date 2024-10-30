45ebd477e09f6469f919ac06a2d4a76e7757a05e
pmprete <pmprete@gmail.com>
2024-10-30 17:23:27 -0300

    Added token metadata

--- src/createMintWithTransferFee.ts
+++ src/createMintWithTransferFee.ts
@@ -14,31 +14,62 @@ import {
   getMintLen,
   TOKEN_2022_PROGRAM_ID,
   createInitializeTransferFeeConfigInstruction,
+  TYPE_SIZE,
+  LENGTH_SIZE,
+  createInitializeMetadataPointerInstruction,
 } from "@solana/spl-token";
- 
+
+import {
+  createInitializeInstruction,
+  createUpdateFieldInstruction,
+  createRemoveKeyInstruction,
+  pack,
+  TokenMetadata,
+} from "@solana/spl-token-metadata";
+
+import dotenv from "dotenv";
+dotenv.config();
+
 export async function createMintWithTransferFee(
   connection: Connection,
   mintAuthority: PublicKey,
   transferFeeConfigAuthority: PublicKey,
   withdrawWithheldAuthority: PublicKey,
+  updateMetadataAuthority: PublicKey,
   payer: Keypair, // account that has funds to pay for the transaction
   mintKeypair: Keypair, // mint account, tokens come from here
   decimals: number,
   feeBasisPoints: number,
   maxFee: bigint,
 ): Promise<TransactionSignature> {
-  const extensions = [ExtensionType.TransferFeeConfig];
-  const mintLength = getMintLen(extensions);
+
+  // Metadata to store in Mint Account
+  const metaData: TokenMetadata = {
+    updateAuthority: updateMetadataAuthority,
+    mint: mintKeypair.publicKey,
+    name: process.env.TOKEN_NAME || "OPOS",
+    symbol: process.env.TOKEN_SYMBOL || "OPOS",
+    uri: process.env.TOKEN_URI || "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/DeveloperPortal/metadata.json",
+    additionalMetadata: [],
+  };
+
+  // Size of MetadataExtension 2 bytes for type, 2 bytes for length
+  const metadataExtension = TYPE_SIZE + LENGTH_SIZE;
+  // Size of metadata
+  const metadataLen = pack(metaData).length;
+
+  const extensions = [ExtensionType.TransferFeeConfig, ExtensionType.MetadataPointer];
+  const mintLen = getMintLen(extensions);
  
   const mintLamports =
-    await connection.getMinimumBalanceForRentExemption(mintLength);
+    await connection.getMinimumBalanceForRentExemption(mintLen + metadataExtension + metadataLen);
  
   console.log("Creating a transaction with transfer fee instruction...");
 
   const createAccountInstruction = SystemProgram.createAccount({
       fromPubkey: payer.publicKey,
       newAccountPubkey: mintKeypair.publicKey,
-      space: mintLength,
+      space: mintLen,
       lamports: mintLamports,
       programId: TOKEN_2022_PROGRAM_ID,
     });
@@ -51,6 +82,7 @@ export async function createMintWithTransferFee(
     maxFee,
     TOKEN_2022_PROGRAM_ID,
   );
+
   const initializeMintInstruction = createInitializeMintInstruction(
     mintKeypair.publicKey,
     decimals,
@@ -58,11 +90,32 @@ export async function createMintWithTransferFee(
     null,
     TOKEN_2022_PROGRAM_ID,
   );
+  // Instruction to initialize the MetadataPointer Extension
+  const initializeMetadataPointerInstruction = createInitializeMetadataPointerInstruction(
+    mintKeypair.publicKey, // Mint Account address
+    updateMetadataAuthority, // Authority that can set the metadata address
+    mintKeypair.publicKey, // Account address that holds the metadata
+    TOKEN_2022_PROGRAM_ID,
+  );
+  // Instruction to initialize Metadata Account data
+  const initializeMetadataInstruction = createInitializeInstruction({
+    programId: TOKEN_2022_PROGRAM_ID, // Token Extension Program as Metadata Program
+    metadata: mintKeypair.publicKey, // Account address that holds the metadata
+    updateAuthority: updateMetadataAuthority, // Authority that can update the metadata
+    mint: mintKeypair.publicKey, // Mint Account address
+    mintAuthority: mintAuthority, // Designated Mint Authority
+    name: metaData.name,
+    symbol: metaData.symbol,
+    uri: metaData.uri,
+  });
 
   const mintTransaction = new Transaction().add(
     createAccountInstruction,
+    initializeMetadataPointerInstruction,
+    // note: the above instructions are required before initializing the mint
     initializeTransferFeeConfig,
-    initializeMintInstruction
+    initializeMintInstruction,
+    initializeMetadataInstruction,
   );
  
   console.log("Sending transaction...");
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,48 @@
-import { clusterApiUrl, Connection, Keypair, PublicKey } from "@solana/web3.js";
-import { addKeypairToEnvFile, getKeypairFromEnvironment, initializeKeypair } from "@solana-developers/helpers";
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+import { addKeypairToEnvFile, getKeypairFromEnvironment, airdropIfRequired } from "@solana-developers/helpers";
 import { transferCheckedWithFee } from "@solana/spl-token";
 import { createMintWithTransferFee } from "./createMintWithTransferFee";
+import dotenv from "dotenv";
+dotenv.config();
  
+
+const cluster = "devnet";
 /**
  * Create a connection and initialize a keypair if one doesn't already exists.
  * If a keypair exists, airdrop a SOL token if needed.
  */
-const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-// Next, we use a funded account that will deploy the contracts
-const payer = getKeypairFromEnvironment('PRIVATE_KEY');
-//const payer = await initializeKeypair(connection);
+const connection = new Connection(clusterApiUrl(cluster), "confirmed");
+// Next, we use an account that will deploy the contracts
+let payer = getKeypairFromEnvironment('PRIVATE_KEY');
+// If account does not exists create it
+if (!payer) {
+    payer = Keypair.generate();
+    await addKeypairToEnvFile(payer, 'PRIVATE_KEY');
+}
 console.log(`Payer public key: ${payer.publicKey.toBase58()}`);
 
+// Ask for airdrop if needed on devnet
+if (cluster === "devnet") {
+    const newBalance = await airdropIfRequired(
+        connection,
+        payer.publicKey,
+        0.5 * LAMPORTS_PER_SOL,
+        1 * LAMPORTS_PER_SOL,
+      );
+      console.log(`Payer balance: ${newBalance}`);
+}
+
+
 // We will have an authority account that will be able to update the program, update fees and mint authority
-const authority = new PublicKey("BbsHjhFUTJj17zghuLLAYpQqnuq97LVUrxEoSq5Nj6HP");
-console.log(`Authority public key: ${authority.toBase58()}`);
+const mintAuthority = process.env.MINT_AUTHORITY ? new PublicKey(process.env.MINT_AUTHORITY) : payer.publicKey;
+console.log(`mintAuthority public key: ${mintAuthority.toBase58()}`);
+const transferFeeConfigAuthority = process.env.TRANSFER_FEE_CONFIG_AUTHORITY ? new PublicKey(process.env.TRANSFER_FEE_CONFIG_AUTHORITY) : payer.publicKey;
+console.log(`transferFeeConfigAuthority public key: ${transferFeeConfigAuthority.toBase58()}`);
+const withdrawWithheldAuthority = process.env.WITHDRAW_AUTHORITY ? new PublicKey(process.env.WITHDRAW_AUTHORITY) : payer.publicKey;
+console.log(`withdrawWithheldAuthority public key: ${withdrawWithheldAuthority.toBase58()}`);
+const updateMetadataAuthority = process.env.UPDATE_METADATA_AUTHORITY ? new PublicKey(process.env.UPDATE_METADATA_AUTHORITY) : payer.publicKey;
+console.log(`withdrawWithheldAuthority public key: ${updateMetadataAuthority.toBase58()}`);
+
  
 // mint account, this will be the token address
 const mintKeypair = Keypair.generate();
@@ -24,19 +51,20 @@ const mint = mintKeypair.publicKey;
 console.log("\nmint public key: " + mintKeypair.publicKey.toBase58() + "\n\n");
 
 
-const decimals = 9;
+const decimals = Number(process.env.DECIMALS || 8);
 // fee to collect on transfers in basis points, equivalent to 1%
 // Don't use ur brain, use https://www.omnicalculator.com/finance/basis-point
-const feeBasisPoints = 100;
+const feeBasisPoints = Number(process.env.FEE_BASIS_POINTS || 100);
 // maximum fee to collect on transfers
-const maxFee = BigInt("18446744073709551615"); // Max u64 value
+const maxFee = BigInt(process.env.MAX_FEE || "18446744073709551615"); // Max u64 value
  
 // CREATE MINT WITH TRANSFER FEE
 const mintTransactionSig = await createMintWithTransferFee(
     connection,
-    authority,
-    authority,
-    authority,
+    mintAuthority,
+    transferFeeConfigAuthority,
+    withdrawWithheldAuthority,
+    updateMetadataAuthority,
     payer,
     mintKeypair,
     decimals,
